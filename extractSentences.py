e8771a6303e6eec8e6d78b579d702eb58f137603
Jayanta47 <jayantasadhu4557@gmail.com>
2023-07-28 20:46:15 +0600

    extract sentence file fixed for creation of folder for different files result

--- extractSentences.py
+++ extractSentences.py
@@ -1,6 +1,7 @@
 from dataScrapper import * 
 import pandas as pd
 from wordFinder import *
+import json
 
 def get_all_files(directory):
     all_files = []
@@ -23,22 +24,17 @@ if __name__ == "__main__":
     suffixList = getSuffixList("possibleSuffix.txt")
 
     filesList = []
+    dir = False
     if sys.argv[1] == "-f":
         filesList = sys.argv[2:]
     elif sys.argv[1] == "-dir":
+        dir = True
         for i in range(2, len(sys.argv)):
             files = get_all_files(sys.argv[i])
             filesList.extend(files)
     else:
         print("Invalid argument")
         exit(1)
-
-    # stemmerCore = RafiStemmer()
-    # stemmerWrapper = StemmerRK(stemmerCore)
-
-    # scrapper = DataScrapper(filesList, stemmerWrapper, weatWordList)
-    # evaluator = WordEvaluatorRegexSuffix(weatWordList, suffixList)
-    import json
 
     weatWordDict = json.load(open('weatWordsWithSuffix.jsonl', 'r', encoding='utf-8'))
     weatWordList = list(weatWordDict.keys())
@@ -59,7 +55,25 @@ if __name__ == "__main__":
     saveData["Index"] = range(len(weatWordList))
     weatWordDF = pd.DataFrame(saveData)
 
-    sentencesDF = pd.DataFrame({"Index": range(len(sentenceList)), "Sentence": sentenceList, "SourceFile": filesIndexList})
+    if dir:
+        sentencesDF = pd.DataFrame({"Index": range(len(sentenceList)), "Sentence": sentenceList, "SourceFile": filesIndexList})
+    else:
+        sentencesDF = pd.DataFrame({"Index": range(len(sentenceList)), "Sentence": sentenceList})
     
-    weatWordDF.to_csv("weatWordsSentences.csv", index=False)
-    sentencesDF.to_csv("sentences.csv", index=False)
+    if dir:
+        weatWordDF.to_csv("weatWordsSentences.csv", index=False)
+        sentencesDF.to_csv("sentences.csv", index=False)
+    else:
+        import os
+        folderName = "_".join([fileName.split("/")[-1].split(".")[0] for fileName in filesList])
+        print(folderName)
+        os.makedirs("./" + folderName, exist_ok=True)
+        weatWordDF.to_csv(f"./{folderName}/weatWordsSentences.csv", index=False)
+        sentencesDF.to_csv(f"./{folderName}/sentences.csv", index=False)
+
+
+# stemmerCore = RafiStemmer()
+# stemmerWrapper = StemmerRK(stemmerCore)
+
+# scrapper = DataScrapper(filesList, stemmerWrapper, weatWordList)
+# evaluator = WordEvaluatorRegexSuffix(weatWordList, suffixList)
